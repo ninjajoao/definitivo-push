e6519213abd92d899ec5368f20ea96f1c8fc4ebe
ninjajoao <juc_tav@hotmail.com>
2018-08-22 12:52:22 -0300

    main  channge

--- server.js
+++ server.js
@@ -3,4 +3,8 @@ const app = express()
 
 app.use(express.static('public'))
 
+app.get('/users/:id', function (req, res) {
+    res.sendStatus(404)
+})
+
 app.listen(process.env.PORT || 8080, () => console.log("All good. Take a breath."))
