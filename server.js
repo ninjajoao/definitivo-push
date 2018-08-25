defb2d0e3d5ce5da2b1224cdd23000e7f5fe4228
ninjajoao <juc_tav@hotmail.com>
2018-08-25 11:44:35 -0300

    test update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // set up handlebars view engine
 var handlebars = require('express3-handlebars')
-.create({ defaultLayout:'main' });
+	.create({ defaultLayout:'main',extname: '.hb' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
