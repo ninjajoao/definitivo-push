5acf3787f345bf5f2230353fb01b4e953d4b57c8
ninjajoao <juc_tav@hotmail.com>
2018-08-26 01:38:43 -0300

    'script'

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ app.post('/gerar', urlencodedParser, function(req, res) {
 			resultadoNome: name,
 			resultadoPosts: posts,
 			resultadoId: id,
-			layout: 'resultLayout' }, res.redirect('/gerar/' + id));
+			layout: 'resultLayout' });
+		res.redirect('/gerar' + id);
 		console.log('\x1b[32m', '\n\nNome: ' + name + ' - Posts: ' + posts + ' - Id: ' + id + "\n\n", '\x1b[0m'); 
 	}
 	else {
